c73afc93f8de0bbe9d7fa28ab8452cbc2702ba46
laihb <laihb@dcircledevelop.onmicrosoft.com>
2021-03-24 11:43:26 +0700

    Flexbox

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import PresentationalComponent from './PresentationalComponent';
 
-export default function App() {
+const Home = (props) => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.redbox} />
+      <View style={styles.bluebox} />
+      <View style={styles.blackbox} />
     </View>
-  );
+  )
 }
+
+export default Home
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+    backgroundColor: 'grey',
+    height: 600
   },
-});
+  redbox: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red'
+  },
+  bluebox: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'blue'
+  },
+  blackbox: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'black'
+  }
+})
